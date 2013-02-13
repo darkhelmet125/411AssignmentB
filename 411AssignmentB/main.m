9cf65809911cbf06d674fae3bdaef768ba4edaaf
Matthew Short <darkhelmet125@yahoo.com>
2013-02-12 16:01:09 -0800

    Initial Commit

---
+++ 411AssignmentB/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  411AssignmentB
+//
+//  Created by William Short on 2/12/13.
+//  Copyright (c) 2013 William Short. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
